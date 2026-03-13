c735ecb51154c5235dfec052a784854f1c82db1f
dante6686-pp <dante6686@gmail.com>
2026-03-13 19:40:10 +0100

    Update quizzes.js

--- quizzes.js
+++ quizzes.js
@@ -229,3 +229,14 @@ if (searchInput) {
 }
 
 renderAll();
+
+function shuffleArray(array) {
+  const arr = [...array];
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
+}
